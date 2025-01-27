7a1d9c91c6408c10a5d72d83951d04b9d08c9ecb
renato matos <renato.matos79@gmail.com>
2025-01-27 12:09:33 +0000

    adding coverage

--- FRONT/vitest.config.ts
+++ FRONT/vitest.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
         include: ['src/**/*.test.tsx'],
         globals: true,
         environment: 'jsdom',
-        setupFiles: './setup.ts'
+        setupFiles: './setup.ts',
+        coverage: {
+            provider: 'v8',
+            include: ['src/**/*.tsx'],
+            exclude: ['src/**/*.test.tsx', 'src/types', 'src/configs', 'src/constants'],
+            reporter: ['text', 'html'],
+        },
     },
   })
